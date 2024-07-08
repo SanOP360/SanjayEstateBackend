e4cf896d2e83d291d7888722ee06315868a14231
SanOP360 <sanjayl2001alm@gmail.com>
2024-07-08 11:54:23 +0530

    added some production changes

--- api/index.js
+++ api/index.js
@@ -6,10 +6,12 @@ const userRouter = require("./routes/user.route");
 const authRouter = require("./routes/auth.route");
 const listingRouter=require("./routes/listing.route")
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
 dotenv.config();
 
 const app = express();
+
 
 app.use(
   cors({
@@ -33,6 +35,13 @@ mongoose
 app.use("/user", userRouter);
 app.use("/api/auth", authRouter);
 app.use("/api/listing",listingRouter)
+
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 
 app.use((err, req, res, next) => {
