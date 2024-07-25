bf06e61cbcdf4e5331697b8ccee5af6bf1a44e9c
SanOP360 <sanjayl2001alm@gmail.com>
2024-07-25 12:03:37 +0530

    change

--- index.js
+++ index.js
@@ -14,15 +14,22 @@ const app = express();
 
 const allowedOrigins = ["http://localhost:5173"];
 
+// app.use(
+//   cors({
+//     origin: function (origin, callback) {
+//       if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//         callback(null, true);
+//       } else {
+//         callback(new Error("Not allowed by CORS"));
+//       }
+//     },
+//     credentials: true,
+//   })
+// );
+
 app.use(
   cors({
-    origin: function (origin, callback) {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
